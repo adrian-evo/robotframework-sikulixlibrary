2fef683d41f4799e700763ccc88ad5f869b4e1ce
Paul van Haren <paul.vanharen@zonnet.nl>
2023-02-09 16:16:48 +0100

    Minor improvement

--- SikuliXLibrary/sikulixdebug.py
+++ SikuliXLibrary/sikulixdebug.py
@@ -8,13 +8,13 @@ class SikuliXDebug(SikuliXJClass):
         SikuliX Debug class
     '''
     @keyword
-    def set_debug(self, variable, value):
+    def set_debug(self, value):
         '''
-        Sets the debug level of the SikuliX core engine. 
+        Sets the debug level of the SikuliX core engine. This data is logged to the console (stdout).
         Default is 0, more output is generated using level 3.
         
         Example
         | Set Debug | 3 |
         '''
-        SikuliXJClass.Debug.setGlobalDebug(value)
+        SikuliXJClass.Debug.setGlobalDebug(int(value))
         
